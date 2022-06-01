45051e1bbdf029d989b54a179e2e2ca3b29807e0
Jason Ji <uberjason@gmail.com>
2022-05-31 21:29:11 -0400

    Initial commit - a sample project showing a bug with NavigationLink inside Forms, probably related to cell reuse somehow.

--- NavigationLinkListFun/ContentView.swift
+++ NavigationLinkListFun/ContentView.swift
@@ -7,10 +7,58 @@
 
 import SwiftUI
 
+struct Item: Identifiable {
+    let id = UUID()
+    let name: String
+}
+
+class ViewModel: ObservableObject {
+    @Published var items = [Item(name: "Initial Item")]
+    @Published var linkActive = false
+}
+
 struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section("Some Content (Ignore - Just for Spacing)") {
+                    TextEditor(text: Binding.constant(""))
+                        .frame(minHeight: 400)
+                }
+                Section("Items") {
+                    NavigationLink(
+                        isActive: $viewModel.linkActive,
+                        destination: {
+                            DetailView(items: $viewModel.items)
+                        },
+                        label: {
+                            Button {
+                                viewModel.linkActive = true
+                            } label: {
+                                Text("Add Items")
+                            }
+                        }
+                    )
+                    ForEach(viewModel.items) { item in
+                        Text(item.name)
+                    }
+                    
+                }
+            }
+            .navigationTitle("Navigation Woes")
+        }
+    }
+}
+
+struct DetailView: View {
+    @Binding var items: [Item]
+    
+    var body: some View {
+        Button("Add an item") {
+            items.append(Item(name: "An item"))
+        }
     }
 }
 
