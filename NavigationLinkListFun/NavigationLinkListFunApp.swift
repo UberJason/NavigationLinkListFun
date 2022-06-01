74186eb22c9197e69b35beb936f3af62962fbc1f
Jason Ji <uberjason@gmail.com>
2022-05-31 21:16:48 -0400

    Initial Commit

---
+++ NavigationLinkListFun/NavigationLinkListFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationLinkListFunApp.swift
+//  NavigationLinkListFun
+//
+//  Created by Jason Ji on 5/31/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationLinkListFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
